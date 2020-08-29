5fff52829c51ab69743f65be5533234af9f926e1
Matt Diesel <M@ttDiesel.co.uk>
2020-08-28 17:02:49 -0700

    Fixed: analysis not recalculating on merge.

--- csvmerge.py
+++ csvmerge.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import argparse
 import sys
+from gpxanalysis import *
 
 parser = argparse.ArgumentParser(description='Create an image of a track overlayed on a map')
 
@@ -14,6 +15,8 @@ args = parser.parse_args()
 df = pd.concat([pd.read_csv(f, index_col='time', parse_dates=True) for f in args.files], axis=0)
 
 df.sort_index(inplace=True)
+df = gpxAnalyse(df)
+
 
 if not args.output:
     df.to_csv(sys.stdout)
