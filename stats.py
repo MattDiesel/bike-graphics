bdab8aa141e142e8fa352b7dd6fd0468205ead11
Matt Diesel <M@ttDiesel.co.uk>
2020-08-28 17:03:33 -0700

    Fixed: Longer than 24 hour moving times not formatting correctly

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
+import datetime
 
 import argparse
 
@@ -14,18 +15,16 @@ df = pd.read_csv(args.file, index_col='time', parse_dates=True)
 
 print("Distance: {0:.2f}km".format(df['s'].iat[-1]))
 
-print("Moving Time: {0}".format(time.strftime('%H:%M:%S', time.gmtime(df['elapsed'].iat[-1]))))
+print("Moving Time: {0}".format(str(datetime.timedelta(seconds=df['elapsed'].iat[-1]))))
 
 time_series = df.index.to_series()
 print("Total Time: {0}".format(time_series.iat[-1] - time_series.iat[0]))
 
 
 v = df['vel'].loc[np.equal(df['moving'], 1)]
-# print("Average Speed: {0:.2f}km/h".format(v.mean()))
 print("Average Speed: {0:.2f}km/h".format( df['s'].iat[-1] * 3600 / df['elapsed'].iat[-1] ))
 
 print("Max Speed: {0:.2f}km/h".format(v.max()))
-
 
 print("Ascent: {0:.0f}m".format(df['asc'].iat[-1]))
 print("Descent: {0:.0f}m".format(df['desc'].iat[-1]))
