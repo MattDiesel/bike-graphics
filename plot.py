86760e7d63e1b471928f3ec6d207486dac16dfec
Matt Diesel <M@ttDiesel.co.uk>
2020-08-28 17:03:03 -0700

    Made smoothing optional for plot.

--- plot.py
+++ plot.py
@@ -9,19 +9,23 @@ parser = argparse.ArgumentParser(description='Create a graph')
 parser.add_argument('file', help='CSV file to use')
 parser.add_argument('column', help='Column to plot')
 parser.add_argument('-o', '--output', help='Output PNG image.')
-parser.add_argument('-s', '--smooth', type=int, default=20, help='Output PNG image.')
+parser.add_argument('-s', '--smooth', type=int, help='Output PNG image.')
 
 args = parser.parse_args()
 
 df = pd.read_csv(args.file, index_col='time', parse_dates=True)
 
-d = df[args.column]
-df[args.column + "'"] = np.convolve( \
-    np.concatenate((np.ones(args.smooth)*d.iat[0], d, np.ones(args.smooth)*d.iat[-1])), \
-    np.ones(args.smooth * 2 + 1) / (args.smooth * 2 + 1), \
-    mode="valid")
+ploc = args.column
+if args.smooth:
+    d = df[args.column]
+    df[args.column + "'"] = np.convolve( \
+        np.concatenate((np.ones(args.smooth)*d.iat[0], d, np.ones(args.smooth)*d.iat[-1])), \
+        np.ones(args.smooth * 2 + 1) / (args.smooth * 2 + 1), \
+        mode="valid")
+    ploc = args.column + "'"
+    
 
-df.plot('elapsed', args.column + "'")
+df.plot('elapsed', ploc)
 
 if not args.output:
     plt.show()
